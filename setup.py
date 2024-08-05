feaf151ab515a8a81172756438c6667e6d7fe4a1
crazyyanchao <yanchaoma@foxmail.com>
2024-08-05 19:54:50 +0800

    keys _custom_sort

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='llmcompiler',
-    version="1.0.6",
+    version="1.0.7",
     author="Yc-Ma",
     author_email="yanchaoma@foxmail.com",
     description='LLMCompiler',
