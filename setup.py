a45e5f09f88c9871c0c88ecd6e1033a0a9cff177
crazyyanchao <yanchaoma@foxmail.com>
2024-10-23 13:28:11 +0800

    upgrade

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='llmcompiler',
-    version="1.2.13",
+    version="1.2.14",
     author="Yc-Ma",
     author_email="yanchaoma@foxmail.com",
     description='LLMCompiler',
